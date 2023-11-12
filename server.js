88b2d180b5b1e9d227415ffb9488a475e3d49eb7
Artemon-x58 <artem1145819@gmail.com>
2023-11-12 11:02:36 +0100

    update

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-const app = require('./app')
+const app = require("./app");
 
-const {DB_HOST, PORT = 3000} = process.env;
+const DB_HOST =
+  "mongodb+srv://Artem:cciGv4Q3zfSdcihu@cluster0.ohfkusp.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT)
+    app.listen(3000);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
-  })
+  });
